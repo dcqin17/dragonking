ef56da36c5bfa83ecb37137dd70646cf107bfbe4
Raoul W <rrrlw@users.noreply.github.com>
2018-12-08 12:41:18 -0500

    Adjusted code so that entire setuptools library isn't imported

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-import setuptools
+from setuptools import setup
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setuptools.setup(
+setup(
     name="dragonking",
     version="0.1.0",
     author="Daniel Qin, Raoul R. Wadhwa, Peter Erdi",
