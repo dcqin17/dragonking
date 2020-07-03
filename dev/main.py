2205f324ddcbeec2b98a5d90e50c7b74c127871c
Michael Orwin <michaelxorwin@gmail.com>
2020-07-03 10:26:54 -0400

    working inward and outward

--- dev/main.py
+++ dev/main.py
@@ -7,6 +7,7 @@ Created on Fri Jun 26 17:38:55 2020
 import numpy as np
 import pandas as pd
 import argparse
+import dragonking as dk
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='DragonKing Program')
@@ -19,8 +20,40 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-    dk_file1 = pd.read_csv(args.data_file)
-    dkvalues1 = dk_file1.iloc[ : , : ].values
-    dkvalues1 = np.sort(dkvalues1.transpose()[::-1][0])[::-1]
+    dataframe = pd.read_csv(args.data_file)
+    df_values = dataframe.iloc[ : , : ].values
+    sorted_values = np.sort(df_values.transpose()[0])[::-1]
 
-    print(dkvalues1)
+    # Only runs ss_stat currently and is not using args.test_statistic
+    outliers1 = dk.inward(
+        teststat = dk.ss_stat,
+        data = sorted_values,
+        r = args.outlier_upperbound,
+        m = args.num_outliers,
+        alpha = args.alpha
+    )
+
+    print(outliers1)
+
+    outliers2 = dk.outward(
+        teststat = dk.ss_stat,
+        data = sorted_values,
+        r = args.outlier_upperbound,
+        m = args.num_outliers,
+        alpha = args.alpha
+    )
+
+    print(outliers2)
+
+
+    '''if args.procedure == "inward":
+        inward(teststat, sorted_values, args.r, args.m, args.alpha=.15)
+    elif args.procedure == "outward":
+        inward(args.test_statistic, sorted_values, r, m, alpha=.15)
+    else:
+        print("invalid procedure provided; options are 'inward' and 'outward'")'''
+
+
+    #print(sorted_values)
+
+# python main.py -d sample1.csv -r 3 -m 3 -p inward -t ss_stat -a 0.15
