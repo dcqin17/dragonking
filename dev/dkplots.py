6e44d4127e0cd2e826b1f32472ac3da119b14645
dcqin17 <danielqin99@gmail.com>
2020-07-21 12:00:15 -0400

    'message'

--- dev/dkplots.py
+++ dev/dkplots.py
@@ -1,12 +1,15 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.special import betainc
 
-def dkplot(pops):
-    xs = np.log10(pops)
-    ys = np.log10(np.cumsum(xs[::-1])[::-1] / sum(xs))
+def dkplot(pops,b,h):
+    xs = pops
+    Fs = 1-np.exp(-b*np.log(xs-h))
+    #ys = np.cumsum(xs[::-1])[::-1] / sum(xs)
+    ys = 1-np.cumsum(Fs) / sum(xs)
     #ys[-1] = np.log10(0.0015)
-    plt.scatter(xs, ys)
-    print(ys)
+    plt.scatter(pops, ys)
+    print(Fs)
     plt.show()
 
 if __name__ == '__main__':
@@ -22,7 +25,7 @@ if __name__ == '__main__':
 
     GBdata = np.array(GBdata, dtype = np.float)
     #GBdata[::-1].sort()
-    GBdata.sort()
+    GBdata[::-1].sort()
 
     #dkplot(GBdata,1.502,50300)
-    dkplot(GBdata)
+    dkplot(GBdata,1.502,50300)
